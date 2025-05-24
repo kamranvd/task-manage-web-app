7b4dfd4529f02eaa800715fcb847c9ebdd21f961
Kamran <kamran@lasysadmin.com>
2025-05-24 00:44:48 -0700

    slight change to homepage url to see if hashrouter works

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 
 import './App.css';
-import { BrowserRouter, Link } from "react-router-dom";
+import { HashRouter, Link } from "react-router-dom";
+import Home from './components/Home/Home';
+import ToDoList from './components/ToDoList/ToDoList';
+import Contacts from './components/Contacts/Contacts';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h2>Task Management App</h2>
-       <BrowserRouter>
+       <HashRouter>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/todolist">To do list</Link></li>
             <li><Link to="/contacts">Contacts</Link></li>
           </ul>
-       </BrowserRouter>
-
+       </HashRouter>
+       <Home />
+       <ToDoList />
+       <Contacts />
       </header>
     </div>
   );
