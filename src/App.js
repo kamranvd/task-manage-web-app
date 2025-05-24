21da0c973a3c44dd23362243433a04ab8c0c5730
Kamran <kamran@lasysadmin.com>
2025-05-24 00:05:55 -0700

    Creating the required structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import { BrowserRouter, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h2>Simple heading</h2>
+       <h2>Task Management App</h2>
+       <BrowserRouter>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/todolist">To do list</Link></li>
+            <li><Link to="/contacts">Contacts</Link></li>
+          </ul>
+       </BrowserRouter>
+
       </header>
     </div>
   );
